e36678ce180909a17f61bea24bf6e09822cd0dc0
David Ojha <davidojha753@gmail.com>
2025-01-10 12:09:11 +0545

    contact

--- forms/contact.php
+++ forms/contact.php
@@ -1,23 +1,30 @@
 <?php
-  $receiving_email_address = 'davidojha753@gmail.com';
+// Specifing the receiving email address
+$receiving_email_address = 'davidojha753@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Check if the "PHP Email Form" Library exists, then include it or stop execution
+$php_email_form = '../assets/vendor/php-email-form/php-email-form.php';
+if (file_exists($php_email_form)) {
+    include($php_email_form);
+} else {
+    die('Unable to load the "PHP Email Form" Library!');
+}
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// Create a new instance of PHP_Email_Form
+$contact = new PHP_Email_Form();
+$contact->ajax = true;
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// Set up email details
+$contact->to = $receiving_email_address;
+$contact->from_name = isset($_POST['name']) ? $_POST['name'] : '';
+$contact->from_email = isset($_POST['email']) ? $_POST['email'] : '';
+$contact->subject = isset($_POST['subject']) ? $_POST['subject'] : 'No Subject';
 
-  echo $contact->send();
+// Add messages to the email
+$contact->add_message($contact->from_name, 'From');
+$contact->add_message($contact->from_email, 'Email');
+$contact->add_message(isset($_POST['message']) ? $_POST['message'] : '', 'Message', 10);
+
+// Send the email and echo the result
+echo $contact->send();
 ?>
